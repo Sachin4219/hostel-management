a1d6242dd7098331b102019893a5411792179672
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2022-08-27 20:04:34 +0530

    fixing router

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes> 
-            <Route exact path='/' element={<h1>Welcome Home</h1>}> </Route>
-            <Route path='profile' element={<h1>Profile</h1>}> </Route>
-            <Route exact path='dashboard' element={<h1>Dashboard</h1>}> </Route>
-            <Route exact path='complaints' element={<Complaints/>}> </Route>
-        </Routes> 
-      </Router>
-    </div>
+    <Router>
+      <Routes> 
+          <Route exact path='/' element={<h1>Welcome Home</h1>}> </Route>
+          <Route exact path='profile' element={<h1>Profile</h1>}> </Route>
+          <Route exact path='dashboard' element={<h1>Dashboard</h1>}> </Route>
+          <Route exact path='complaints' element={<Complaints/>}> </Route>
+      </Routes> 
+    </Router>
   );
 }
 
