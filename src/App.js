859200c79f70160070536cea46abe08e23cc45ac
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2022-08-27 19:56:26 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Complaints from './pages/Complaints';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    {/* <Navbar></Navbar> */}
-    {/* <Table/>  */}
-    
-    <Complaints></Complaints>
+    <div>
+      <Router>
+        <Routes> 
+            <Route exact path='/' element={<h1>Welcome Home</h1>}> </Route>
+            <Route exact path='/profile' element={<h1>Profile</h1>}> </Route>
+            <Route exact path='/dashboard' element={<h1>Dashboard</h1>}> </Route>
+            <Route exact path='/complaints' element={<Complaints/>}> </Route>
+        </Routes> 
+      </Router>
     </div>
   );
 }
